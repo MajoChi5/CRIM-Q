26c7933ee33093bef21c6b9d8bdec61c5b82138a
MajoChi5 <A01712974@tec.mx>
2025-06-10 20:09:28 -0600

    Update System.cpp

--- System.cpp
+++ System.cpp
@@ -3,12 +3,17 @@
 #include "Crime.h"
 #include "Police.h"
 
-void System::addUser(User* user) { users.push_back(user); }
-void System::addCrime(Crime* crime) { crimes.push_back(crime); }
-void System::assignToPolice(Police* officer, Crime* crime) { officer->assignCrime(crime); }
-const vector<User*>& System::getUsers() const { return users; }
-const vector<Crime*>& System::getCrimes() const { return crimes; }
-System::~System() {
+void System::addUser(User* user) { users.push_back(user); } // Add user to the system
+
+void System::addCrime(Crime* crime) { crimes.push_back(crime); }// Add crime to the system
+
+void System::assignToPolice(Police* officer, Crime* crime) { officer->assignCrime(crime); } // Assign crime to police officer
+
+const vector<User*>& System::getUsers() const { return users; } // Get all users
+
+const vector<Crime*>& System::getCrimes() const { return crimes; }// Get all crimes
+
+System::~System() {// Destructor to clean memory
     for (User* u : users) delete u;
     for (Crime* c : crimes) delete c;
 }
